7ec785f0ca9c40b550cc6d00a5dd04e42264b25c
Mansour Abdoli <mabdoli@fullerton.edu>
2023-11-05 20:24:34 -0800

    Fixing the rMask() for matrixes, adding twist, and resolving memory issue.

--- maskPrj/rDataGen.R
+++ maskPrj/rDataGen.R
@@ -1,5 +1,7 @@
 # Generating noisy data
 # Load True Masks
+
+if(!grepl(pattern = 'maskPrj$', getwd())) setwd('maskPrj')
 if(file.exists('mask2m.rda')){
   load('mask2m.rda')
 } else{
@@ -9,22 +11,32 @@ if(file.exists('mask2m.rda')){
   save(mask2m, file = 'mask2m.rda')
 }
 
-setwd('maskPrj')
 source('rMaskGen.R')
 mdim <- dim(mask2m)
 k <- prod(mdim[1:2])
 pos <- k2ij(mdim[1], 1:k)
 x <- mask2m
 dim(x) <- c(k, mdim[3])
+rm(mask2m)
 
 nrnd <- 10
 Y <- rep(1:k, nrnd)
 X <- x
+tiltDeg <- rep(0, k)
+twistDeg <- rep(0, k)
+phShift <- rep(0, k)
+
 for(i in 2:nrnd){
-  X <- rbind(X, rMask(x, tilt_err = .1, mask_err = 1))
+  kMask <- rMaskmmm(x, tilt_err = .1, twist_err = 5, mask_err = 2,
+                 orientation_err = 5)
+  X <- rbind(X, kMask$masks)
+  tiltDeg <- rbind(kMask$tiltDeg)
+  twistDeg <- rbind(twistDeg, kMask$twistDeg)
+  phShift <- rbind(phShift, kMask$phShift)
 }
 
-rm(x, mask2m)
+rm(x, kMaks)
+
 loss <- function(y, yhat, method=c("distance", "mismatch"), xy=pos){
   mthd <- match.arg(method[1], c("distance", "mismatch"))
   if(length(mthd)==0)
@@ -35,5 +47,6 @@ loss <- function(y, yhat, method=c("distance", "mismatch"), xy=pos){
     1.0*(y!=round(yhat,0))
   }
 }
-maskData <- list(x=X, y=Y, pos=pos, loss=loss)
+maskData <- list(x=X, y=Y, pos=pos, loss=loss, tiltDeg=tiltDeg,
+                 twistDeg=twistDeg, phShift=phShift)
 save(maskData, file = 'MaskData.rda')
--- maskPrj/rMaskGen.R
+++ maskPrj/rMaskGen.R
@@ -4,25 +4,102 @@ k2ij <- function(nx, k){
   cbind((k-1)%%nx+1, (k-1)%/%nx+1)
 }
 
-rMask <- function(x # True Mask
-                  ,tilt_err=0.1 # Tilt error
-                  ,mask_err=1
-                  ,az_cover=2*pi # The azimuth coverage of mask
-){
-  # Number of points on one mask
-  nAz <- if(is.matrix(x)) dim(x)[2] else length(x)
-  # Azimuth angles
-  az <- seq(0, az_cover, length.out = nAz)
-  tiltAngle=rnorm(1, 0, tilt_err)
-  noise_tilt=cos(az-runif(1, 0, 2*pi))*tiltAngle
-  noise_mask=rnorm(nAz)*mask_err
-
-  if(is.matrix(x)){
-    x+matrix(1,nrow = dim(x)[1], ncol = 1)%*%
-      matrix(noise_tilt+noise_mask, nrow=1)
-  } else
-    x+noise_tilt+noise_mask
+shiftK <- function(x, K){
+  n <- length(x)
+  K <- K %% n
+  if(K==0) x else
+    if(K>0) x[c((1+K):n, 1:K)] else
+      if(K<0) x[c((n-K+1):n, 1:(n-K))]
 }
+
+rMask <- function(x # True Mask
+                  ,tilt_err=0.1 # N(0, Tilt error (SD)) in degrees
+                  ,twist_err=1 # N(0, twist angle (SD)) in degrees / Or uniform(-180, 180)
+                  ,mask_err=1 # N(0, mask_err (SD)) in degrees
+                  ,orientation_err=5 # standard deviation in degrees
+                  ,az_cover=360 # The azimuth coverage of mask in degrees
+                  ,clipLim=c(-Inf, +Inf) # Values for clipping masks
+                  ,clipValNA = FALSE # If clipped masks should be treated as NAs
+){
+  #browser()
+  if(!is.matrix(x)){
+    x <- matrix(x, nrow=1)
+  }
+  # Number of masks
+  m <- dim(x)[1]
+  # Number of points on one mask
+  nAz <- dim(x)[2]
+
+  # Azimuth angles
+  az <- seq(0, az_cover/360*2*pi, length.out = nAz)
+
+  # Adding orientation error
+  phShift <- round(rnorm(m, 0, orientation_err)*360/nAz, 0)
+  x <- lapply(1:m, \(k) shiftK(x[k,], phShift[k]))
+  x <- do.call('rbind', x)
+  # Adding tilt and noise
+  tiltAngle <- rnorm(m, 0, tilt_err)
+  twistAngle <- if(twist_err==-1) runif(m, -180, 180) else rnorm(m, 0, twist_err/180*pi)
+  noise_twist <- matrix(twistAngle, nrow=m) %*% matrix(sin(az), nrow=1)
+  tilt_twist <- lapply(twistAngle, \(a) cos(az-a))
+  tilt_twist <- do.call('rbind', tilt_twist)
+  noise_tilt=diag(tiltAngle, nrow = m) %*% tilt_twist - noise_twist
+  noise_mask=matrix(rnorm(m*nAz)*mask_err, nrow = m)
+  out <- x+noise_tilt+noise_mask
+  # Limit
+  out[which(out<clipLim[1])] <- if(clipValNA) NA else clipLim[1]
+  out[which(out>clipLim[2])] <- if(clipValNA) NA else clipLim[2]
+  #
+  list(masks=out, tiltDeg=tiltAngle, twistDeg=twistAngle, phShift)
+}
+
+# Solving the Memory issue
+rMaskmmm <- function(x # True Mask
+                  ,tilt_err=0.1 # N(0, Tilt error (SD)) in degrees
+                  ,twist_err=1 # N(0, twist angle (SD)) in degrees / Or uniform(-180, 180)
+                  ,mask_err=1 # N(0, mask_err (SD)) in degrees
+                  ,orientation_err=5 # standard deviation in degrees
+                  ,az_cover=360 # The azimuth coverage of mask in degrees
+                  ,clipLim=c(-Inf, +Inf) # Values for clipping masks
+                  ,clipValNA = FALSE # If clipped masks should be treated as NAs
+){
+  #browser()
+  if(!is.matrix(x)){
+    x <- matrix(x, nrow=1)
+  }
+  # Number of masks
+  m <- dim(x)[1]
+  # Number of points on one mask
+  nAz <- dim(x)[2]
+
+  # Azimuth angles
+  az <- seq(0, az_cover/360*2*pi, length.out = nAz)
+
+  # Adding orientation error
+  phShift <- round(rnorm(m, 0, orientation_err)*360/nAz, 0)
+  x <- lapply(1:m, \(k) shiftK(x[k,], phShift[k]))
+  x <- do.call('rbind', x)
+  # Adding tilt and twist
+  tiltAngle <- rnorm(m, 0, tilt_err)
+  twistAngle <- if(twist_err==-1) runif(m, -180, 180) else rnorm(m, 0, twist_err/180*pi)
+  # Adding noise
+  x <- lapply(
+    1:m,
+    function(k){
+      v <- shiftK(x[k,], phShift[k])
+      noise_tilt <- tiltAngle[k]*cos(az-twistAngle[k]) - twistAngle[k]*sin(az)
+      noise_mask <- rnorm(nAz)*mask_err
+      v + noise_tilt + noise_mask
+    })
+  x <- do.call('rbind', x)
+
+  # Limit
+  x[which(x<clipLim[1])] <- if(clipValNA) NA else clipLim[1]
+  x[which(x>clipLim[2])] <- if(clipValNA) NA else clipLim[2]
+  #
+  list(masks=x, tiltDeg=tiltAngle, twistDeg=twistAngle, phShift)
+}
+
 
 ## Example
 if(FALSE){
